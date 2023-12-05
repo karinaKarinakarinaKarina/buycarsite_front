69d7eac165799f1dd8b01dd16f02794a0de84b23
haribo0372 <sasha530000@gmail.com>
2023-12-05 16:50:21 +0300

    add registration and login processing

--- src/components/registration.js
+++ src/components/registration.js
@@ -1,30 +1,33 @@
 const form = document.getElementById('registrationForm');
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
+if (form != null){
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
 
-  const formData = new FormData(form);
+      const formData = new FormData(form);
 
-  fetch('http://localhost:5000/registration', {
-    method: 'POST',
-    body: formData
-  })
-  .then(response => response.json())
-  .then(data => registration(data))
-  .catch(error => console.error(error));
-});
-
+      fetch('http://localhost:5000/registration', {
+        method: 'POST',
+        body: formData
+      })
+      .then(response => response.json())
+      .then(data => registration(data))
+      .catch(error => console.error(error));
+    });
+}
 function registration(data){
     console.log(data)
     let name = data.username.toString()
-    let message = data.message.toString()
-    if (message == ''){
-       location.href = 'login.html';
-       console.log(name)
+    if (name != ''){
+       location.href = 'http://localhost:8000/src/pages/login.html';
     }
     else{
-       let HTMLData = `<p>Error: ${data.message}</p>`
-       document.getElementById("data").innerHTML = HTMLData
-       console.log("NOT GOOD")
+       while (document.getElementById("errorText") != null){
+        document.getElementById("errorText").remove();
+       }
+       let tagA = document.createElement("a");
+       tagA.id = "errorText";
+       tagA.textContent = `${data.message}`
+       document.getElementById("error").appendChild(tagA);
     }
 }
